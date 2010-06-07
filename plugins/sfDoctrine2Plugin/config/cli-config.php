33ea807c47372cf53ae191dc5f39686ed3d0e7b1
Henrique Boaventura <hboaventura@gmail.com>
2010-06-06 21:48:58 -0300

    retirado o plugin para doctrine2

--- plugins/sfDoctrine2Plugin/config/cli-config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$configuration = sfProjectConfiguration::getActive();
-$databaseManager = new sfDatabaseManager($configuration);
-$names = $databaseManager->getNames();
-$em = $databaseManager->getDatabase(end($names))->getEntityManager();
-$args = array();
